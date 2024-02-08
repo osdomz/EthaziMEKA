7a16b589311026417bf054717c5d409e21107a47
osdomz <osdom.dev@hotmail.com>
2024-02-08 22:21:11 +0100

    ip mariadb

--- Backend/Config/bd.php
+++ Backend/Config/bd.php
@@ -3,7 +3,7 @@
 namespace N00_Config;
 //TODO: Moficiar ruta de produccion
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.1.11');
 define ('DB_NAME','banca');
 define ('DB_USER','root');
 define ('DB_PASS','');
