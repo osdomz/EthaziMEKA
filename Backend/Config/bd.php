c498c94e5ad7caa31b027c861540458ae70a6036
osdomz <osdom.dev@hotmail.com>
2024-02-08 21:31:35 +0100

    re commit

--- Backend/Config/bd.php
+++ Backend/Config/bd.php
@@ -3,18 +3,9 @@
 namespace N00_Config;
 //TODO: Moficiar ruta de produccion
 
+define('DB_HOST', 'localhost');
+define ('DB_NAME','banca');
+define ('DB_USER','root');
+define ('DB_PASS','');
 
-
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    // Acceso desde localhost
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'banca');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-} else if ($_SERVER['SERVER_NAME'] === 'virtualcash.zerbitzaria.net'){
-    // Otro servidor
-    define('DB_HOST', 'virtualcash.zerbitzaria.net');
-    define('DB_NAME', 'banca');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-}
+?>
