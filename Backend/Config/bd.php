682005d6eb3d1079ba20a3955848037a765fbad1
Christian <osdom.dev@hotmail.com>
2024-02-09 09:20:44 +0100

    cambios bd mariadb ultimo

--- Backend/Config/bd.php
+++ Backend/Config/bd.php
@@ -3,7 +3,7 @@
 namespace N00_Config;
 //TODO: Moficiar ruta de produccion
 
-define('DB_HOST', 'grupo1.com'); //localhost
+define('DB_HOST', 'db.grupo1.com'); //localhost
 define ('DB_NAME','banca_definitiva'); //banca
 define ('DB_USER','root');
 define ('DB_PASS','zornotza');
